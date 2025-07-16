d2400c87f473103ccc83ff51b77e014649948717
unknown <muhammadmiftahusshiddiq@gmail.com>
2025-07-16 20:37:44 +0700

    Browser Route

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,7 +3,9 @@ import style from "./Footer.module.css"
 function Footer() {
     return (
         <div>
+            <footer className={style.footer}>
 
+            </footer>
         </div>
     )
 }
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -3,7 +3,9 @@ import style from "./Navbar.module.css"
 function Navbar() {
     return (
         <div>
+            <nav className={style.nav}>
 
+            </nav>
         </div>
     )
 }
