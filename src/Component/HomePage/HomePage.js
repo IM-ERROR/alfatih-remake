dd6ef93de44a2627358f8dbe50dac269b0e4fd5e
IM-ERROR <dhiyaulhaq80624@gmail.com>
2025-07-17 22:39:39 +0700

    add home

--- src/Component/HomePage/HomePage.js
+++ src/Component/HomePage/HomePage.js
@@ -2,8 +2,13 @@ import style from "./HomePage.module.css"
 
 function HomePage() {
     return (
-        <div>
-            <img className={style.img_logo} src='./image-alfatih/Home.png' alt='image/Home' />
+        <div className={style.container_home}>
+            <div className={style.heros}>
+                <img className={style.img_home} src='./image-alfatih/Home.png' alt='image/Home' />
+                <div className={style.arrow}>
+                    <img className={style.img_arrow} src='./image-alfatih/Arrow.png' alt='image/Arrow' />
+                </div>
+            </div>
 
         </div>
     )
